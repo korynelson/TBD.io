b786d02955939f71857c7e11c1340387b54457d5
birolezgin <b.ezginksf@gmail.com>
2020-09-26 13:59:59 -0700

    schema update

--- schema.sql
+++ schema.sql
@@ -2,3 +2,12 @@
 DROP DATABASE IF EXISTS streamer;
 -- Creates the "blogger" database --
 CREATE DATABASE streamer;
+
+USE streamer;
+
+CREATE TABLE videoId
+(
+	name VARCHAR(30)
+    );
+    
+INSERT INTO videoID (name) VALUES ("hLMjnm1Bm_Y");
