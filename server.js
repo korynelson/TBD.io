c00c981991db45e904df69b1a9e1a4d9738c3772
Kory Nelson <kory.nelson99@gmail.com>
2020-09-25 21:10:23 -0700

    added config.json

---
+++ server.js
@@ -0,0 +1,6 @@
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session");
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
