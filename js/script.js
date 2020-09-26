ef9c4541efdff2e20cdb180d48eea48c1147327f
Kory Nelson <kory.nelson99@gmail.com>
2020-09-25 20:42:40 -0700

    add searchbtn listener and store value in local storage

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 //youtube api key
-AIzaSyAQmv6tfX2O--XdCmHzgIP-RJ_KcoMUjxA
+//AIzaSyAQmv6tfX2O--XdCmHzgIP-RJ_KcoMUjxA
+
+
+$(document).ready(function(){
+    $("#searchBtn").on("click",function(event){
+        event.preventDefault();
+        window.localStorage.setItem("movieSearch",JSON.stringify($("#searchInput").val()));
+    })
+})
